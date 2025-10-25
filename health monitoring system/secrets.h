a64f817dfeb099f2b661f95da217bf125168ae1b
Keerathan-dilip <keerathan10@gmail.com>
2025-10-25 16:31:16 +0530

    Add files via upload

---
+++ health monitoring system/secrets.h
@@ -0,0 +1,6 @@
+// secrets.h
+#define BLYNK_TEMPLATE_ID   "TMPL3OG0Q36oT"
+#define BLYNK_TEMPLATE_NAME "project 2"
+#define BLYNK_AUTH_TOKEN    "it8Dx2_WTyhBTGMKAFwMcXTaiNKQV66C"
+#define WIFI_SSID           "Wokwi-GUEST"
+#define WIFI_PASS           ""
